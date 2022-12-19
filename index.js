79bd020d1fe4e55762a457a3f2dcd7949a912a91
ambrose-kibet <ambrosekibet36@gmail.com>
2022-12-19 19:51:21 +0300

    add mobile menu fuctionality

---
+++ index.js
@@ -0,0 +1,16 @@
+const closeBtn = document.querySelector(".close-btn");
+const openBtn = document.querySelector(".nav-btn");
+const sidebar = document.querySelector(".sidebar");
+const navLinks = document.querySelectorAll(".nav-link");
+openBtn.addEventListener("click", () => {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    sidebar.classList.remove("show-sidebar");
+  });
+});
