f7851e1eabe288e12aab61124f1638a5abdba822
ambrose-kibet <ambrosekibet36@gmail.com>
2022-12-21 11:23:12 +0300

    add project cards dynamically

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const collapseBtn = document.querySelector('.close-btn');
 const expandBtn = document.querySelector('.nav-btn');
 const sidebar = document.querySelector('.sidebar');
 const navLinks = document.querySelectorAll('.nav-link');
-const openModal = document.querySelectorAll('.pop-toggler');
 const modal = document.querySelector('.modal-overlay');
 const closeModal = document.querySelector('.close-modal');
 const modalContainer = document.querySelector('.modal-header');
+const projectsContainer = document.getElementById('projects');
 const projects = [
   {
     name: 'stories',
@@ -68,20 +68,93 @@ navLinks.forEach((link) => {
     sidebar.classList.remove('show-sidebar');
   });
 });
-openModal.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const projectName = e.target.parentElement.parentElement.dataset.name;
-      const {
+
+window.addEventListener('DOMContentLoaded', () => {
+  projects.map((item) => {
+    const {
+      name,
       title,
       info,
       tech,
       src,
-      live,
-      sourceCode,
-    } = projects.find(
-      (item) => item.name === projectName,
-    );
-    const modalContent = `<h3 class="card-title">
+
+    } = item;
+    if (name === 'stories') {
+      const article = document.createElement('article');
+      article.classList.add('project-card');
+      article.classList.add('card');
+      article.dataset.name = name;
+
+      article.innerHTML = `<div class="card-header">
+              <img
+                src="${src}"
+                alt="${title}"
+                class="card-header-img"
+              />
+            </div>
+            <div data-name="stories">
+              <div class="card-body">
+                <h2 class="card-title">${title}</h2>
+                <p class="card-info">
+                     ${info}
+                    </p>
+                <ul class="tech-container">
+                ${tech.map((tech) => `<li class="tech">${tech}</li>`)}
+                </ul>
+              </div>
+              <div class="card-footer">
+                <button class="btn pop-toggler">See Project</button>
+              </div>
+            </div>
+          `;
+      return projectsContainer.appendChild(article);
+    }
+    const container = document.createElement('div');
+    container.classList.add('project-cards-container');
+    const article = document.createElement('article');
+    article.classList.add('project-card-variant');
+    article.classList.add(name);
+    article.classList.add('card');
+    article.dataset.name = name;
+
+    article.innerHTML = `<div class="card-variant-body">
+                <h2 class="card-title">${title}</h2>
+                <p class="card-info">
+                 ${info}
+                </p>
+                <ul class="tech-container">
+                ${tech.map((tech) => `<li class="tech">${tech}</li>`)}
+                </ul>
+              </div>
+              <div class="card-variant-footer">
+                <button class="btn btn-block pop-toggler">See Project</button>
+              </div>`;
+
+    return projectsContainer.appendChild(article);
+  });
+  const children = [...projectsContainer.children];
+  console.log(children);
+  children.forEach((child) => {
+    if (child.classList.contains('card')) {
+      let projButton;
+      const projName = child.dataset.name;
+      if (child.classList.contains('project-card')) {
+        projButton = child.children[1].children[1].children[0];
+      } else {
+        projButton = child.children[1].children[0];
+      }
+      projButton.addEventListener('click', () => {
+        const {
+          title,
+          info,
+          tech,
+          src,
+          live,
+          sourceCode,
+        } = projects.find(
+          (item) => item.name === projName,
+        );
+        const modalContent = `<h3 class="card-title">
                   ${title}
                 </h3>
                 <ul class="tech-container">
@@ -111,8 +184,10 @@ openModal.forEach((btn) => {
                 </div>
               `;
 
-    modalContainer.innerHTML = modalContent;
-    modal.classList.add('show-modal');
+        modalContainer.innerHTML = modalContent;
+        modal.classList.add('show-modal');
+      });
+    }
   });
 });
 
